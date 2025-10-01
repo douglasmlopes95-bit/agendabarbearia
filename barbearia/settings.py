2f245f341a8ab5fde441dcb8850e107ea520d54a
douglasmlopes95-bit <douglasmlopes95@gmail.com>
2025-09-30 22:07:22 -0300

    Atualizar settings.py para Render

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------
 SECRET_KEY = 'nutxip-bicvo5-meJgud'  # Troque para uma chave secreta segura em produção
 DEBUG = True
-ALLOWED_HOSTS = []  # Adicione domínios em produção
+ALLOWED_HOSTS = ['agendabarbearia.onrender.com']  # Adicione outros domínios se necessário
 
 # -----------------------
 # Aplicativos instalados
