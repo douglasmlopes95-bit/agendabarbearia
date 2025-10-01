4c12379346f26d61ac21a43716052d0a3d6437d2
douglasmlopes95-bit <douglasmlopes95@gmail.com>
2025-09-30 22:24:00 -0300

    Atualização para Render

--- barbearia/urls.py
+++ barbearia/urls.py
@@ -1,20 +1,22 @@
-# project/urls.py
+# barbearia/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     # Painel administrativo do Django
     path("admin/", admin.site.urls),
 
     # URLs do app principal "agenda"
-    path("", include(("agenda.urls", "agenda"), namespace="agenda")),
+    path("agenda/", include(("agenda.urls", "agenda"), namespace="agenda")),
 
-    # URLs para autenticação de usuários (descomente se for usar login/logout padrão do Django)
-    # path("accounts/", include("django.contrib.auth.urls")),
+    # Redireciona a raiz '/' para '/agenda/'
+    path("", lambda request: redirect("agenda/")),
 ]
 
-# Servir arquivos de mídia (como logos) durante o desenvolvimento
+# Servir arquivos de mídia e estáticos (somente durante desenvolvimento)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
